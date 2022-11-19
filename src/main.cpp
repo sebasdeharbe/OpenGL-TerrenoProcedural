0512e72ff1a6a16467e3758c91c8d6c9eb269076
JuanKeiner <116316134+JuanKeiner@users.noreply.github.com>
2022-11-19 20:11:23 -0300

    Update 20221119.3
    Saqué partes del código en funciones aparte

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ typedef struct {
 	int fact = 1; 					//escala de ruido
 	int freq = 1;			 		//frecuencia
 	int amp = 1;					//amplitud
-	int seed = 65;           		//seed para el srand()
+	int seed = 0;           		//seed para el srand()
 	float persistency = 2.f;       	//
 	float lacunarity = 0.5f;       	//
 //	float 
@@ -46,7 +46,36 @@ float interpolacionLineal(float x1,float x2, float v1,float v2, float tx){
 	float total=fabs(x2-x1);
 	return (sumV1+sumV2)/total;
 }
-
+	
+void generarOctava(vector<vector<float>> &nuevaOctava, float amplitud, int tamanioSubdivision){
+	
+	for(int i=0;i<=parametros.tamanioMapa;i+=tamanioSubdivision){
+		
+		for(int j=0;j<=parametros.tamanioMapa;j+=tamanioSubdivision){
+			
+			//Crear nodos
+			nuevaOctava[i][j] = amplitud*((float)rand()/RAND_MAX);
+			
+			//Interpolar puntos interiores
+			if(i>=tamanioSubdivision && j>=tamanioSubdivision){
+				
+				for(int a=i-tamanioSubdivision; a<=i; a++){
+					for(int b=j-tamanioSubdivision; b<=j; b++){
+						
+						int x1=i-tamanioSubdivision;
+						int x2=i;
+						int y1=j-tamanioSubdivision;
+						int y2=j;
+						nuevaOctava[a][b] = interpolacionBilineal(x1, y1, x2, y2, nuevaOctava[x1][y1], nuevaOctava[x2][y1], nuevaOctava[x1][y2], nuevaOctava[x2][y2], a, b);
+					}
+				} 
+				
+			}
+		}
+		
+	}
+}
+	
 vector<vector<float>> createNoiseMap(){
 	srand(parametros.seed); 
 	vector<vector<float>> noiseMap(parametros.tamanioMapa+1,vector<float>(parametros.tamanioMapa+1));
@@ -62,32 +91,10 @@ vector<vector<float>> createNoiseMap(){
 	while(parametros.tamanioMapa%tamanioSubdivision) cout<<endl<<"Rompe el tamanio de subdiv"<<endl<<endl;
 	
 	///PRIMERA OCTAVA
-	for(int i=0;i<=parametros.tamanioMapa;i+=tamanioSubdivision){
-		
-		for(int j=0;j<=parametros.tamanioMapa;j+=tamanioSubdivision){
-			
-			//Crear nodos
-			noiseMap[i][j] = amplitud*((float)rand()/RAND_MAX);
-			
-			//Interpolar puntos interiores
-			if(i>=tamanioSubdivision && j>=tamanioSubdivision){
-				
-				for(int a=i-tamanioSubdivision; a<=i; a++){
-					for(int b=j-tamanioSubdivision; b<=j; b++){
-						
-						int x1=i-tamanioSubdivision;
-						int x2=i;
-						int y1=j-tamanioSubdivision;
-						int y2=j;
-						noiseMap[a][b] = interpolacionBilineal(x1, y1, x2, y2, noiseMap[x1][y1], noiseMap[x2][y1], noiseMap[x1][y2], noiseMap[x2][y2], a, b);
-					}
-				} 
-				
-			}
-		}
-		
-	}
+	generarOctava(noiseMap, amplitud, tamanioSubdivision);
 	
+	
+	///LAS OTRAS
 	for(int o=1;o<parametros.numeroDeOctavas;o++) { 
 		
 		frecuencia *= parametros.persistency;
@@ -95,34 +102,7 @@ vector<vector<float>> createNoiseMap(){
 		tamanioSubdivision = parametros.tamanioMapa/frecuencia;
 		
 		vector<vector<float>> nuevaOctava(parametros.tamanioMapa+1,vector<float>(parametros.tamanioMapa+1));
-		
-		
-		
-		for(int i=0;i<=parametros.tamanioMapa;i+=tamanioSubdivision){
-			
-			for(int j=0;j<=parametros.tamanioMapa;j+=tamanioSubdivision){
-				
-				//Crear nodos
-				nuevaOctava[i][j] = amplitud*((float)rand()/RAND_MAX);
-				
-				//Interpolar puntos interiores
-				if(i>=tamanioSubdivision && j>=tamanioSubdivision){
-					
-					for(int a=i-tamanioSubdivision; a<=i; a++){
-						for(int b=j-tamanioSubdivision; b<=j; b++){
-							
-							int x1=i-tamanioSubdivision;
-							int x2=i;
-							int y1=j-tamanioSubdivision;
-							int y2=j;
-							nuevaOctava[a][b] = interpolacionBilineal(x1, y1, x2, y2, nuevaOctava[x1][y1], nuevaOctava[x2][y1], nuevaOctava[x1][y2], nuevaOctava[x2][y2], a, b);
-						}
-					} 
-					
-				}
-			}
-			
-		}
+		generarOctava(nuevaOctava, amplitud, tamanioSubdivision);
 		
 		for(int m=0; m<noiseMap.size();m++) { 
 			for(int n=0; n<noiseMap[m].size();n++) { 
@@ -136,7 +116,37 @@ vector<vector<float>> createNoiseMap(){
 	
 }
 
-void modifyMesh(const std::vector<glm::vec3> &v,std::vector<glm::vec3> &vertices,std::vector<glm::vec2> &coords, vector<vector<float>> &noiseMap) {
+float interpolarAltura(int i, float xMin, float xMax, float zMin, float zMax, const std::vector<glm::vec3> &v, vector<vector<float>> &noiseMap ) {
+	
+	float deltaX = abs(xMax - xMin);
+	float deltaZ = abs(zMax - zMin);
+	
+	float xRuido = ((v[i].x-(float)xMin)/((float)deltaX)) * (float)parametros.tamanioMapa;
+	float zRuido = ((v[i].z-(float)zMin)/((float)deltaZ)) * (float)parametros.tamanioMapa;
+	
+	float xInterpolacionMin = floor(xRuido);
+	float xInterpolacionMax = ceil(xRuido);
+	
+	float zInterpolacionMin = floor(zRuido);
+	float zInterpolacionMax = ceil(zRuido);
+	
+	float valorInterpolado = 0;
+	if(xInterpolacionMax == xInterpolacionMin){ //Si el punto a interpolar se encuentra perfectamente entre 2 puntos del mapa de ruido se hace una interpolación lineal directamente.
+		if(zInterpolacionMax == zInterpolacionMin) valorInterpolado = noiseMap[xInterpolacionMin][zInterpolacionMin]; //Si el punto se encuentra donde hay un valor en el mapa de ruido nisiquiera se interpola nada. Tomamos el valor y listo
+		else valorInterpolado = interpolacionLineal(zInterpolacionMin, zInterpolacionMax, noiseMap[xInterpolacionMin][zInterpolacionMin], noiseMap[xInterpolacionMin][zInterpolacionMax], zRuido);
+	}else if(zInterpolacionMax == zInterpolacionMin){
+		valorInterpolado = interpolacionLineal(xInterpolacionMin, xInterpolacionMax, noiseMap[xInterpolacionMin][zInterpolacionMax], noiseMap[xInterpolacionMax][zInterpolacionMax], xRuido);
+	}else{
+		valorInterpolado = interpolacionBilineal(xInterpolacionMin, zInterpolacionMin, xInterpolacionMax, zInterpolacionMax,
+												 noiseMap[xInterpolacionMin][zInterpolacionMin], noiseMap[xInterpolacionMax][zInterpolacionMin],
+													 noiseMap[xInterpolacionMin][zInterpolacionMax], noiseMap[xInterpolacionMax][zInterpolacionMax],
+														 xRuido, zRuido);
+	}
+	
+	return valorInterpolado;
+}
+	
+void modifyMesh(const std::vector<glm::vec3> &v, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &coords, vector<vector<float>> &noiseMap) {
 	
 	vertices.resize(v.size());
 	coords.resize(v.size());
@@ -153,38 +163,16 @@ void modifyMesh(const std::vector<glm::vec3> &v,std::vector<glm::vec3> &vertices
 		if(v[i].z>zMax) zMax = v[i].z;
 	}
 	
-	float deltaX = abs(xMax - xMin);
-	float deltaZ = abs(zMax - zMin);
-	
+	float valorInterpolado = 0.f;
 	for(int i=0;i<v.size();i++) { 
-		
-		float xRuido = ((v[i].x-(float)xMin)/((float)deltaX)) * (float)parametros.tamanioMapa;
-		float zRuido = ((v[i].z-(float)zMin)/((float)deltaZ)) * (float)parametros.tamanioMapa;
-		
-		float xInterpolacionMin = floor(xRuido);
-		float xInterpolacionMax = ceil(xRuido);
-		
-		float zInterpolacionMin = floor(zRuido);
-		float zInterpolacionMax = ceil(zRuido);
-		
-		float valorInterpolado = 0;
-		if(xInterpolacionMax == xInterpolacionMin){ //Si el punto a interpolar se encuentra perfectamente entre 2 puntos del mapa de ruido se hace una interpolación lineal directamente.
-			if(zInterpolacionMax == zInterpolacionMin) valorInterpolado = noiseMap[xInterpolacionMin][zInterpolacionMin]; //Si el punto se encuentra donde hay un valor en el mapa de ruido nisiquiera se interpola nada. Tomamos el valor y listo
-			else valorInterpolado = interpolacionLineal(zInterpolacionMin, zInterpolacionMax, noiseMap[xInterpolacionMin][zInterpolacionMin], noiseMap[xInterpolacionMin][zInterpolacionMax], zRuido);
-		}else if(zInterpolacionMax == zInterpolacionMin){
-			valorInterpolado = interpolacionLineal(xInterpolacionMin, xInterpolacionMax, noiseMap[xInterpolacionMin][zInterpolacionMax], noiseMap[xInterpolacionMax][zInterpolacionMax], xRuido);
-		}else{
-			valorInterpolado = interpolacionBilineal(xInterpolacionMin, zInterpolacionMin, xInterpolacionMax, zInterpolacionMax,
-													noiseMap[xInterpolacionMin][zInterpolacionMin], noiseMap[xInterpolacionMax][zInterpolacionMin],
-													noiseMap[xInterpolacionMin][zInterpolacionMax], noiseMap[xInterpolacionMax][zInterpolacionMax],
-													xRuido, zRuido);
-		}
+		valorInterpolado = interpolarAltura(i, xMin, xMax, zMin, zMax, v, noiseMap); //Saqué toda la interpolación a una función
 		
 		vertices[i] = glm::vec3(v[i].x,valorInterpolado*0.3f,v[i].z);
 		
-		float s = min(vertices[i].y*2.f-0.2f,0.999f);
+		float s = vertices[i].y*2.f-0.2f;
 		if(s<0.001f)s=0.001f;
-		float t = 0.1f;
+		if(s>0.999f)s=0.999f;
+		float t = 0.5f;
 		coords[i] = glm::vec2(s,t);
 		
 	}
